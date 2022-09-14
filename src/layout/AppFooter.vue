bff58d4c20979902d2558fbf99b024b69e33cf06
XeonEVO <parin_boorapa@hotmail.com>
2022-09-14 16:39:21 +0700

    [Footer] update

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-footer
+        dark
+        padless
+        color="#252323"
+        height="210px"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        <strong>© {{ new Date().getFullYear() }} Village Co., Ltd.  All Rights Reserved.</strong>
+      </v-col>
+    </v-footer>
+  </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
