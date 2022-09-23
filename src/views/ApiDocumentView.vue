2e70fd3ac0a6bc95bd772d1656e9d669485ef02a
XeonEVO <parin_boorapa@hotmail.com>
2022-09-23 13:31:11 +0700

    [API Document] Separate overview to Overview.vue

--- src/views/ApiDocumentView.vue
+++ src/views/ApiDocumentView.vue
@@ -73,73 +73,7 @@
               </v-sheet>
             </v-col>
           </v-row>
-          <v-row class="mt-8">
-            <v-col>
-              <p class="detailFont">{{ apiDetail.description }}</p>
-            </v-col>
-          </v-row>
-          <v-row class="mt-4">
-            <v-col>
-              <h1 class="subTitleFont">Features</h1>
-              <ul class="detailFont ml-5 mt-4">
-                <li>Lorem ipsum dolor sit amet.</li>
-                <li>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</li>
-                <li>There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...</li>
-              </ul>
-            </v-col>
-          </v-row>
-          <v-row class="mt-4">
-            <v-col>
-              <h1 class="subTitleFont">Parameters</h1>
-              <ul class="ml-5 mt-4">
-                <li class="detailFont">Body</li>
-              </ul>
-              <v-textarea
-                background-color="black"
-                dark
-                height="350px"
-                rounded
-              ></v-textarea>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <p>Parameter content type</p>
-            </v-col>
-            <v-col>
-              <v-select
-                :items="parameters"
-                outlined
-                dense
-              ></v-select>
-            </v-col>
-          </v-row>
-          <v-row class="mt-4">
-            <v-col>
-              <h1 class="subTitleFont">Response</h1>
-              <ul class="ml-5 mt-4">
-                <li class="detailFont">Status Code 200 OK</li>
-              </ul>
-              <v-textarea
-                background-color="black"
-                dark
-                height="350px"
-                rounded
-              ></v-textarea>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <p>Response content type</p>
-            </v-col>
-            <v-col>
-              <v-select
-                :items="responses"
-                outlined
-                dense
-              ></v-select>
-            </v-col>
-          </v-row>
+          <OverviewAPI v-show="true" />
         </v-card>
       </v-col>
     </v-row>
@@ -147,6 +81,8 @@
 </template>
 
 <script>
+
+import OverviewAPI from './components/ApiDocument/OverviewAPI'
 
 export default {
   data: () => ({
@@ -170,11 +106,11 @@ export default {
       method: 'POST',
       path: 'http://village-developer.com/login',
       description: "\"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum\""
-    },
-    parameters: ['application/json', 'application/xml'],
-    status: ['200 OK', '400 Bad Request', '500 Internal error'],
-    responses: ['application/json', 'application/xml']
+    }
   }),
+  components: {
+    OverviewAPI
+  },
   mounted () {
     if (!localStorage.getItem('token')) {
       this.$router.push({ name: 'login' })
---
+++ src/views/components/ApiDocument/OverviewAPI.vue
@@ -0,0 +1,95 @@
+<template>
+    <v-sheet>
+      <v-row class="mt-8">
+          <v-col>
+            <p class="detailFont">{{ apiDetail.description }}</p>
+          </v-col>
+        </v-row>
+        <v-row class="mt-4">
+          <v-col>
+            <h1 class="subTitleFont">Features</h1>
+            <ul class="detailFont ml-5 mt-4">
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</li>
+              <li>There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...</li>
+            </ul>
+          </v-col>
+        </v-row>
+        <v-row class="mt-4">
+          <v-col>
+            <h1 class="subTitleFont">Parameters</h1>
+            <ul class="ml-5 mt-4">
+              <li class="detailFont">Body</li>
+            </ul>
+            <v-textarea
+              background-color="black"
+              dark
+              height="350px"
+              rounded
+            ></v-textarea>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <p>Parameter content type</p>
+          </v-col>
+          <v-col>
+            <v-select
+              :items="parameters"
+              outlined
+              dense
+            ></v-select>
+          </v-col>
+        </v-row>
+        <v-row class="mt-4">
+          <v-col>
+            <h1 class="subTitleFont">Response</h1>
+            <ul class="ml-5 mt-4">
+              <li class="detailFont">Status Code 200 OK</li>
+            </ul>
+            <v-textarea
+              background-color="black"
+              dark
+              height="350px"
+              rounded
+            ></v-textarea>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <p>Response content type</p>
+          </v-col>
+          <v-col>
+            <v-select
+              v-model="parameter"
+              :items="responses"
+              outlined
+              dense
+            ></v-select>
+          </v-col>
+        </v-row>
+    </v-sheet>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'OverviewAPI',
+  data: () => ({
+    parameter: 'application/json',
+    apiDetail: {
+      title: 'Login website',
+      method: 'POST',
+      path: 'http://village-developer.com/login',
+      description: "\"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum\""
+    },
+    parameters: ['application/json', 'application/xml'],
+    status: ['200 OK', '400 Bad Request', '500 Internal error'],
+    responses: ['application/json', 'application/xml']
+  })
+}
+</script>
+
+<style>
+
+</style>
